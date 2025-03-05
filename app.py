9de59024f2d3373e47f426b64b9e63d2562820ee
allencar029 <159507971+allencar029@users.noreply.github.com>
2025-03-05 13:37:55 -0700

    First commit

---
+++ app.py
@@ -0,0 +1,16 @@
+import cv2 
+import time
+import datetime
+
+cap = cv2.VideoCapture(0)
+
+while True:
+    x, frame = cap.read()
+
+    cv2.imshow("Camera", frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+
+cap.release()
+cv2.destroyAllWindows() 
