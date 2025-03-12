bd3daf7802f20a3ae802852dd5b241001b2ccbd9
allencar029 <159507971+allencar029@users.noreply.github.com>
2025-03-11 21:21:04 -0600

    still making changes to make libcam work with opencv

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
 while True:
     x, frame = cap.read()
 
+    if not x:
+        print("Error: Failed to capture image")
+        break
+
     cv2.imshow("Camera", frame)
 
     if cv2.waitKey(1) == ord('q'):
