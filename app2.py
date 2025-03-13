0ac44b28dc3b1a1b50e562dff421b4804a66224d
allencar029 <159507971+allencar029@users.noreply.github.com>
2025-03-12 19:57:16 -0600

    trying to get q key to work

--- app2.py
+++ app2.py
@@ -6,14 +6,16 @@ from picamera2 import Picamera2
 
 subprocess.run('export DISPLAY=:0', shell=True, executable='/bin/bash')
 
+os.environ['DISPLAY'] = ':0'
+
 picam2 = Picamera2()
 picam2.start()
 
 while True:
     frame = picam2.capture_array()
     cv2.imshow("Live Video", frame)
-    key = cv2.waitKey(0) & 0xFF
-    
+    key = cv2.waitKey(1) & 0xFF
+
     if key == ord('q'):
         print("Exiting...")
         break
