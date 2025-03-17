9f5090714dac9ac8cf839dce56e21c83d1de151e
allencar029 <159507971+allencar029@users.noreply.github.com>
2025-03-16 18:25:26 -0600

    adding awb controls to fix blue color of feed

--- app2.py
+++ app2.py
@@ -3,6 +3,7 @@ import numpy as np
 import subprocess
 import os
 from picamera2 import Picamera2 
+from libcamera import controls
 
 subprocess.run('export DISPLAY=:0', shell=True, executable='/bin/bash')
 
@@ -10,12 +11,13 @@ os.environ['DISPLAY'] = ':0'
 
 picam2 = Picamera2()
 picam2.start()
+picam2.set_controls({"AwbMode": "auto"})
 
 while True:
     frame = picam2.capture_array()
     cv2.imshow("Live Video", frame)
+    
     key = cv2.waitKey(1) & 0xFF
-
     if key == ord('q'):
         print("Exiting...")
         break
