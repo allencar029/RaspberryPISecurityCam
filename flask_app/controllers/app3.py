bcb361729ef35946e573bda6f042798f0fa869af
allencar029 <159507971+allencar029@users.noreply.github.com>
2025-03-18 19:28:43 -0600

    adjusting print statement again

--- flask_app/controllers/app3.py
+++ flask_app/controllers/app3.py
@@ -13,7 +13,7 @@ os.environ['DISPLAY'] = ':0'
 
 picam2 = Picamera2()
 picam2.start()
-print(dir(picam2.controls))
+print(picam2.controls.get_libcamera_controls)
 print("controls before:", picam2.controls)
 picam2.set_controls({
     "AwbMode": 1,
