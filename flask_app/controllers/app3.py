48f1b3fa9af4f0b8589c8fcc10aabeb956aaa070
allencar029 <159507971+allencar029@users.noreply.github.com>
2025-04-09 13:06:06 -0600

    adjusting controls differently

--- flask_app/controllers/app3.py
+++ flask_app/controllers/app3.py
@@ -12,21 +12,32 @@ subprocess.run('export DISPLAY=:0', shell=True, executable='/bin/bash')
 os.environ['DISPLAY'] = ':0'
 
 picam2 = Picamera2()
+picam2.stop()
 picam2.start()
+
+time.sleep(2)
+
 print(dir(picam2))
 print("camera controls:", picam2.controls.get_libcamera_controls())
 print("here is the controls:", picam2.camera_controls)
 picam2.set_controls({
     "AwbEnable": False,
+    "AeEnable": False,
     "ColourTemperature": 4500,
     "ColourGains": (1.5, 2.0),
-}, apply_controls_now=True)
+})
 print("controls after:", picam2.controls)
 
 time.sleep(2)
 
 while True:
     frame = picam2.capture_array("main")
+    frame.set_controls({
+        "AwbEnable": False,
+        "AeEnable": False,
+        "ColourTemperature": 4500,
+        "ColourGains": (1.5, 2.0),
+    })
     cv2.imshow("Live Video", frame)
     
     key = cv2.waitKey(1) & 0xFF
