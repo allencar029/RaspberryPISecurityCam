d48c52accf63d39bc90b39df5f234833ede21c68
allencar029 <159507971+allencar029@users.noreply.github.com>
2025-04-09 19:32:38 -0600

    seeing if the issue persists while capturing a single frame

--- flask_app/controllers/app3.py
+++ flask_app/controllers/app3.py
@@ -21,30 +21,30 @@ print(dir(picam2))
 print("camera controls:", picam2.controls.get_libcamera_controls())
 print("here is the controls:", picam2.camera_controls)
 
-while True:
+picam2.set_controls({
+    "AwbEnable": False,
+    "AeEnable": False,
+    "ColourTemperature": 4500,
+    "ColourGains": (1.5, 2.0),
+})
+print("controls after:", picam2.controls)
 
-    picam2.set_controls({
-        "AwbEnable": False,
-        "AeEnable": False,
-        "ColourTemperature": 4500,
-        "ColourGains": (1.5, 2.0),
-    })
-    print("controls after:", picam2.controls)
+time.sleep(2)
 
-    time.sleep(2)
-    frame = picam2.capture_array("main")
-    # frame.set_controls({
-    #     "AwbEnable": False,
-    #     "AeEnable": False,
-    #     "ColourTemperature": 4500,
-    #     "ColourGains": (1.5, 2.0),
-    # })
-    cv2.imshow("Live Video", frame)
+# while True:
+#     frame = picam2.capture_array("main")
+#     cv2.imshow("Live Video", frame)
     
-    key = cv2.waitKey(1) & 0xFF
-    if key == ord('q'):
-        print("Exiting...")
-        break
+#     key = cv2.waitKey(1) & 0xFF
+#     if key == ord('q'):
+#         print("Exiting...")
+#         break
 
+request = picam2.capture_request()
+frame = request.make_array("main")
+request.release()
+
+cv2.imshow("Captured Frame", frame)
+cv2.waitKey(0)
 cv2.destroyAllWindows()
 picam2.stop()
