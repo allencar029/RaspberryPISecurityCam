86d3d105446d07e077d92a0be568a93167371d50
allencar029 <159507971+allencar029@users.noreply.github.com>
2025-04-14 21:18:08 -0600

    removing unnecessary lines of code

--- flask_app/controllers/app3.py
+++ flask_app/controllers/app3.py
@@ -21,23 +21,16 @@ picam2.configure(config)
 
 picam2.start()
 
-# print("here are the controls after starting the cam:", picam2.camera_controls)
-
 time.sleep(2)
 
-picam2.set_controls({
-    "AwbEnable": False,
-    "ColourTemperature": 3500,
-    "ColourGains": (2.5, 0.0),
-})
+# picam2.set_controls({
+#     "AwbEnable": False,
+#     "ColourTemperature": 3500,
+#     "ColourGains": (2.5, 0.0),
+# })
 
 print("sensor format:", picam2.camera_config["main"]["format"])
-# print(dir(picam2))
-# print("camera controls:", picam2.controls.get_libcamera_controls())
-# print("here is the controls:", picam2.camera_controls)
 
-
-# time.sleep(2)
 
 while True:
     frame = picam2.capture_array("main")
@@ -48,11 +41,5 @@ while True:
         print("Exiting...")
         break
 
-# request = picam2.capture_request()
-# frame = request.make_array("main")
-# request.release()
-
-# cv2.imwrite("/home/calle19/test_image.jpg", frame)
-# cv2.imshow("Captured Frame", frame)
 cv2.destroyAllWindows()
 picam2.stop()
