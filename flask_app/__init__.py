2a25663963236d05d89f80436ea80a8539ec1754
allencar029 <159507971+allencar029@users.noreply.github.com>
2025-04-18 16:48:44 -0600

    adjusting the init.py file to have only one instance of app.

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,9 +1,6 @@
 from flask import Flask
 
-def create_app():
-    app = Flask(__name__)
-    app.secret_key = "shhhhh"
+app = Flask(__name__)
+app.secret_key = "shhhhh"
 
-    from flask_app.controllers import app2
-
-    return app
+from flask_app.controllers import app2
